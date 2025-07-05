b7ce5b5350d6c8cb26b3fd90f3f037278555c863
D. Moschokarfi <despinamx16@gmail.com>
2025-07-05 21:57:06 +0300

    feat: added /api/students path for student route calls

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-// const student = require('./routes/student.routes');
+const student = require('./routes/student.routes');
 
 // app.use('/api/auth', auth);
-// app.use('/api/students', student);
-
+app.use('/api/students', student);
+    
 module.exports = app
