9f6eff595ce4cc6a74f58d2104d7d5b9808bffc3
D. Moschokarfi <despinamx16@gmail.com>
2025-07-18 17:48:43 +0300

    feat: routes created for students and classes

---
+++ routes/student.classes.routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router();
+
+const studentClassController = require('../controllers/student.classes.controller');
+
+router.get('/', studentClassController.findAll);
+router.get('/:username', studentClassController.findOne);
+router.post('/', studentClassController.create);
+router.patch('/', studentClassController.update);
+router.delete('/:username/classes/:className', studentClassController.delete);
+router.get('/stats/ects-statistics', studentClassController.ectsStats);
+
+module.exports = router;
