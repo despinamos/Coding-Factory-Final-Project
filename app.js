d6f9f7e2a876263111b83afe03f32cdd6002b65e
D. Moschokarfi <despinamx16@gmail.com>
2025-07-19 19:20:20 +0300

    feat: added path for authentication

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+const auth = require('./routes/auth.routes');
 const student = require('./routes/student.routes');
 const studentClass = require('./routes/student.classes.routes')
 
-// app.use('/api/auth', auth);
+app.use('/api/auth', auth);
 app.use('/api/students', student);
 app.use('/api/student-class', studentClass)
-    
+
 module.exports = app
