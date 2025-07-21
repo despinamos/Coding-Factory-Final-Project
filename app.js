df56b2456a033d3bb7392b0c658e99a546d5d02f
D. Moschokarfi <despinamx16@gmail.com>
2025-07-21 13:51:34 +0300

    feat: created paths for class, studen-class api and swagger docs

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 const auth = require('./routes/auth.routes');
 const student = require('./routes/student.routes');
-const studentClass = require('./routes/student.classes.routes')
+const classRoutes = require('./routes/class.routes');
+const studentClass = require('./routes/student.classes.routes');
 
 app.use('/api/auth', auth);
 app.use('/api/students', student);
-app.use('/api/student-class', studentClass)
+app.use('/api/classes', classRoutes);
+app.use('/api/student-class', studentClass);
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument.options)
+);
 
 module.exports = app
