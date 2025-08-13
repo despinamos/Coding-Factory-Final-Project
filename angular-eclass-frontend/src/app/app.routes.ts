c9d3a84ee9c98c85c7dc46f149a6d81b924107d0
D. Moschokarfi <despinamx16@gmail.com>
2025-08-13 14:23:08 +0300

    feat: created routes for login, user registration, class creation and welcome components

--- angular-eclass-frontend/src/app/app.routes.ts
+++ angular-eclass-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserLogin } from './components/user-login/user-login';
+import { UserRegistration } from './components/user-registration/user-registration';
+import { Welcome } from './components/welcome/welcome';
+import { authGuard } from './shared/guards/auth-guard';
+import { CreateClass } from './components/create-class/create-class';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: UserLogin},
+    {path: 'user-register', component:UserRegistration, canActivate: [authGuard]},
+    {path: 'create-class', component:CreateClass, canActivate: [authGuard]},
+    {path: 'welcome', component:Welcome},
+    {path: '', redirectTo:'/welcome', pathMatch:'full'}
+];
