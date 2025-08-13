dbaab2fbc386e69b1b308db329de9aed86b7a999
D. Moschokarfi <despinamx16@gmail.com>
2025-08-13 14:25:00 +0300

    feat: created interfaces for Student, Credentials and Logged In User

---
+++ angular-eclass-frontend/src/app/shared/interfaces/student-user.ts
@@ -0,0 +1,24 @@
+export interface StudentUser {
+  username: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  age: number;
+  email: string;
+  address: {
+    area: string;
+    road: string
+  };
+  phone: { number: string; type: string }[];
+}
+
+export interface Credentials {
+  username: string;
+  password: string;
+}
+
+export interface LoggedInUser {
+  username: string,
+  email: string,
+  roles: [string]
+}
