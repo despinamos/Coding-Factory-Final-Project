ec769f86fd4e57c86c80920ee4583a70098667c7
D. Moschokarfi <despinamx16@gmail.com>
2025-08-13 14:22:38 +0300

    feat: main app component for frontend

--- angular-eclass-frontend/src/app/app.component.ts
+++ angular-eclass-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderBar } from './components/header-bar/header-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, HeaderBar],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('angular-eclass-frontend');
