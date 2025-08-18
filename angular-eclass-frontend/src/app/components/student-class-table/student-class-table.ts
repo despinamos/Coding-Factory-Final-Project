5948d06ac9bb031056526caa4794c54b2fa64c20
D. Moschokarfi <despinamx16@gmail.com>
2025-08-18 16:41:24 +0300

    feat: component for a student to view the classes he is enrolled in

---
+++ angular-eclass-frontend/src/app/components/student-class-table/student-class-table.ts
@@ -0,0 +1,38 @@
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
+import { SchoolClass } from 'src/app/shared/interfaces/class';
+import { CommonModule } from '@angular/common';
+import { StudentClassService } from 'src/app/shared/services/student-class.service';
+import { StudentUserService } from 'src/app/shared/services/student-user.service';
+
+@Component({
+  selector: 'app-student-class-table',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './student-class-table.html',
+  styleUrl: './student-class-table.css'
+})
+export class StudentClassTable implements OnInit{
+  constructor(private cdr: ChangeDetectorRef) {};
+  classes: SchoolClass[] = [];
+  studentClassService = inject(StudentClassService);
+  studentUserService = inject(StudentUserService);
+  user = this.studentUserService.user$;
+  loading = true;
+
+  ngOnInit(): void {
+    this.studentClassService.getClassesForStudent(this.user()).subscribe({
+      next: (response) => {
+        console.log("Getting classes for student request", response);
+        this.classes = response;
+        console.log("student classes>>", this.classes);
+        this.loading = false;
+        this.cdr.detectChanges(); 
+      },
+      error: (err) => {
+        console.error('Error fetching classes', err);
+        this.loading = false;
+      }
+    });
+  }
+
+}
