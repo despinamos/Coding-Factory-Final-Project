6a5bdbf009e70e90e4e38f9cb3786795cbb13c5b
D. Moschokarfi <despinamx16@gmail.com>
2025-08-18 18:00:52 +0300

    feat: added menu for students

--- angular-eclass-frontend/src/app/components/welcome/welcome.ts
+++ angular-eclass-frontend/src/app/components/welcome/welcome.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { StudentUserService } from 'src/app/shared/services/student-user.service';
 import { MenuList } from '../menu-list/menu-list';
+import { StudentMenu } from '../student-menu/student-menu';
 
 @Component({
   selector: 'app-welcome',
-  imports: [MenuList],
+  imports: [MenuList, StudentMenu],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css'
 })
