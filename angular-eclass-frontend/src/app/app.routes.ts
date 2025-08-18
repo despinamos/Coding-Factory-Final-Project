10dc8dc2e38ee1b9f8d12d862aac62d6c61ac86f
D. Moschokarfi <despinamx16@gmail.com>
2025-08-18 16:58:03 +0300

    feat: added routes for new components and used admin role guard

--- angular-eclass-frontend/src/app/app.routes.ts
+++ angular-eclass-frontend/src/app/app.routes.ts
@@ -6,13 +6,20 @@ import { authGuard } from './shared/guards/auth-guard';
 import { CreateClass } from './components/create-class/create-class';
 import { StudentTable } from './components/student-table/student-table';
 import { ClassTable } from './components/class-table/class-table';
+import { RestrictedContent } from './components/restricted-content/restricted-content';
+import { adminRoleGuard } from './shared/guards/admin-role-guard';
+import { StudentClassTable } from './components/student-class-table/student-class-table';
+import { StudentPersonalInfo } from './components/student-personal-info/student-personal-info';
 
 export const routes: Routes = [
     {path: 'login', component: UserLogin},
-    {path: 'user-register', component:UserRegistration, canActivate: [authGuard]},
-    {path: 'create-class', component:CreateClass, canActivate: [authGuard]},
-    {path: 'all-students', component:StudentTable, canActivate: [authGuard]},
-    {path: 'all-classes', component:ClassTable, canActivate: [authGuard]},
+    {path: 'user-register', component:UserRegistration, canActivate: [authGuard, adminRoleGuard]},
+    {path: 'create-class', component:CreateClass, canActivate: [authGuard, adminRoleGuard]},
+    {path: 'all-students', component:StudentTable, canActivate: [authGuard, adminRoleGuard]},
+    {path: 'all-classes', component:ClassTable, canActivate: [authGuard, adminRoleGuard]},
+    {path: 'student-classes', component:StudentClassTable, canActivate: [authGuard]},
+    {path: 'student-info', component:StudentPersonalInfo, canActivate: [authGuard]},
+    {path: 'restricted-content', component:RestrictedContent},
     {path: 'welcome', component:Welcome},
     {path: '', redirectTo:'/welcome', pathMatch:'full'}
 ];
