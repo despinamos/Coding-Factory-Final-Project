98bb716c1046b182e15e8e91d1cbc7455709da4a
D. Moschokarfi <despinamx16@gmail.com>
2025-08-19 15:18:55 +0300

    fix: used material toolbar

--- angular-eclass-frontend/src/app/components/header-bar/header-bar.ts
+++ angular-eclass-frontend/src/app/components/header-bar/header-bar.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { StudentUserService } from 'src/app/shared/services/student-user.service';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-bar',
-  imports: [RouterLink, MatIconModule],
+  imports: [RouterLink, MatIconModule, MatToolbarModule],
   templateUrl: './header-bar.html',
   styleUrl: './header-bar.css'
 })
