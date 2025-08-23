658197ca6d9fa44dc466ad2c6bdc869cea46a657
D. Moschokarfi <despinamx16@gmail.com>
2025-08-23 13:46:56 +0300

    feat: enroll in class component created

---
+++ angular-eclass-frontend/src/app/components/enroll-in-class/enroll-in-class.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ClassTable } from "../class-table/class-table";
+
+@Component({
+  selector: 'app-enroll-in-class',
+  imports: [ClassTable],
+  templateUrl: './enroll-in-class.html',
+  styleUrl: './enroll-in-class.css'
+})
+export class EnrollInClass {
+
+}
