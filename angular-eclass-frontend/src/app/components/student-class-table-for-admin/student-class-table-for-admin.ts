8354f6df279a5680384e27d425f9ae6f85cb0ce4
D. Moschokarfi <despinamx16@gmail.com>
2025-08-24 21:42:32 +0300

    feat: added option for admin to delete a class from a student

--- angular-eclass-frontend/src/app/components/student-class-table-for-admin/student-class-table-for-admin.ts
+++ angular-eclass-frontend/src/app/components/student-class-table-for-admin/student-class-table-for-admin.ts
@@ -5,10 +5,12 @@ import { StudentUserService } from 'src/app/shared/services/student-user.service
 import { ClassesPerStudent } from 'src/app/shared/interfaces/student-user';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { SchoolClass } from 'src/app/shared/interfaces/class';
 
 @Component({
   selector: 'app-student-class-table-for-admin',
-  imports: [CommonModule, MatButton, RouterLink],
+  imports: [CommonModule, MatButton, MatIconModule, RouterLink],
   templateUrl: './student-class-table-for-admin.html',
   styleUrl: './student-class-table-for-admin.css'
 })
@@ -30,6 +32,22 @@ export class StudentClassTableForAdmin implements OnInit{
       },
       error: (err) => {
         console.error('Error fetching classes', err);
+        this.loading = false;
+      }
+    });
+  }
+
+  deleteClassFromStudent(username: string, clss: SchoolClass) {
+    this.studentClassService.deleteClassFromStudent(username, clss).subscribe({
+      next: (response) => {
+        console.log("Deleting class for student request", response);
+        const indexOfStudent = this.classesPerStudent.findIndex(x => x.username === username);
+        const indexOfDeletedClass = this.classesPerStudent[indexOfStudent].classes.indexOf(clss);
+        this.classesPerStudent[indexOfStudent].classes.splice(indexOfDeletedClass, 1)
+        this.cdr.detectChanges(); 
+      },
+      error: (err) => {
+        console.error('Error deleting class for student', err);
         this.loading = false;
       }
     });
