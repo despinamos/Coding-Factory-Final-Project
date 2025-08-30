08f94466e7dea29f97a8ea885cdc2d56bc0b81c2
D. Moschokarfi <despinamx16@gmail.com>
2025-08-30 14:10:19 +0300

    feat: added button in enroll-class page for student to view his classes without having to go back to the main menu

--- angular-eclass-frontend/src/app/components/enroll-in-class/enroll-in-class.ts
+++ angular-eclass-frontend/src/app/components/enroll-in-class/enroll-in-class.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ClassTable } from "../class-table/class-table";
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-enroll-in-class',
-  imports: [ClassTable],
+  imports: [ClassTable, MatButton, RouterLink],
   templateUrl: './enroll-in-class.html',
   styleUrl: './enroll-in-class.css'
 })
